ad06db305f9735f440cc0aa324894660fd940318
re0ah <re0ah@yandex.ru>
2020-11-29 12:29:44 +0300

    download

---
+++ history_window.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtCore import Qt
+from history import History
+import os
+
+class HistoryWindow(QtWidgets.QMainWindow):
+	def __init__(self):
+		super().__init__(None, Qt.WindowFlags())
+		self.window = uic.loadUi(os.path.join("ui", "history.ui"))
+		self.window.btn_clear.clicked.connect(self.clear)
+		self.display()
+
+	def display(self):
+		"""Обновление поля вывода данных, которые берутся из файла history.txt"""
+		self.window.display.setText(History.load())
+
+	def clear(self):
+		"""Очищает историю и обновляет поле вывода данных"""
+		History.clear()
+		self.display()
